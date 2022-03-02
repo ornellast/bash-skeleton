315dd4fcf6ff5f91a36f5e19f4321579b9650927
Thiago Ornellas <tornellas@gmail.com>
2022-03-02 09:27:57 +0000

    Organising base.sh

--- base.sh
+++ base.sh
@@ -3,42 +3,13 @@
 set -Eeuo pipefail
 trap cleanup SIGINT SIGTERM ERR EXIT
 
-function cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-  # script cleanup here
-  debug "${BLK}cleanup called${NF}"
-}
-
+(return 0 2>/dev/null) && sourced=1 || sourced=0
+readonly sourced
 readonly SCRIPT_ARGS_STR="$@"
-function setup_consts() {
-  readonly SCRIPT_PATH_RELATIVE="$(dirname $0)"
-  readonly SCRIPT_PATH_ABSOLUTE="$(realpath $SCRIPT_PATH_RELATIVE)"
-  readonly SCRIPT_NAME="$(basename $0)"
-  readonly CURRENT_FOLDER="$(basename $PWD)"
-  readonly RUNNING_FROM="${PWD}"
 
-  param=()
-}
-
-function setup_colors() {
-  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-    NOFORMAT='\033[0m'
-    BLACK='\033[1;30m'
-    RED='\033[1;31m'
-    GREEN='\033[1;32m'
-    ORANGE='\033[0;33m'
-    BLUE='\033[1;34m'
-    PURPLE='\033[1;35m'
-    CYAN='\033[0;36m'
-  else
-    NOFORMAT='' BLACK='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN=''
-  fi
-
-  NF="${NOFORMAT}" BLK="${BLACK}" RED="${RED}" GRN="${GREEN}" ORG="${ORANGE}" BLU="${BLUE}" PPL="${PURPLE}" CYN="${CYAN}"
-
-  MAIN_COLOR="${NOFORMAT}"
-  HIGHLIGHT_COLOR="${GREEN}"
-}
+##      #####
+## READ ONLY functions - BEGIN
+##      #####
 
 function assert_var() {
   if [[ ${#1} -eq 0 ]]; then
@@ -47,33 +18,15 @@ function assert_var() {
   fi
 }
 
-function msg() {
-  echo >&2 -e "${1-}"
-}
-
-function throw_error() {
-  local msg=$1
-  local code=${2-1} # default exit status 1
-  msg "[$code] ${RED}${msg}${NF}"
-  exit "$code"
+function cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+  debug "${BLK}cleanup called${NF}"
 }
 
 function debug() {
   echo "${SCRIPT_ARGS_STR}" | grep -qPe "(-v|--verbose)((\s*-)|\$)" && msg "$1"
   return 0
-}
-
-function has_flag() {
-  has_param $@ && return 0 || return 1
-}
-
-function has_param() {
-  local short="-${1}"
-  local long="--${2-NOT_EXISTS}"
-  local regex="((\s*-)|\$)([\s\w]*|\$)"
-  debug "Validating '${RED}${SCRIPT_ARGS_STR}${NF}' against '${RED}(${short}|${long})${regex}${NF}'"
-  echo "${SCRIPT_ARGS_STR}" | grep -qPe "((^|\s+)${short}|${long})${regex}" && return 0
-  return 1
 }
 
 function get_param() {
@@ -99,9 +52,25 @@ function get_param() {
   return 1
 }
 
+function has_flag() {
+  has_param $@ && return 0 || return 1
+}
+
+function has_param() {
+  local short="-${1}"
+  local long="--${2-NOT_EXISTS}"
+  local regex="((\s*-)|\$)([\s\w]*|\$)"
+  debug "Validating '${RED}${SCRIPT_ARGS_STR}${NF}' against '${RED}(${short}|${long})${regex}${NF}'"
+  echo "${SCRIPT_ARGS_STR}" | grep -qPe "((^|\s+)${short}|${long})${regex}" && return 0
+  return 1
+}
+
 function main() {
-  setup_colors
   setup_consts
+  if has_flag 'h' 'help'; then
+    usage
+    exit
+  fi
   initialize_vars
   parse_params $@
   setup_colors
@@ -110,11 +79,66 @@ function main() {
   run
 }
 
-[[ $(type -t usage) != function ]] && usage() {
+function msg() {
+  echo >&2 -e "${1-}"
+}
+
+function setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m'
+    BLACK='\033[1;30m'
+    RED='\033[1;31m'
+    GREEN='\033[1;32m'
+    ORANGE='\033[0;33m'
+    BLUE='\033[1;34m'
+    PURPLE='\033[1;35m'
+    CYAN='\033[0;36m'
+  else
+    NOFORMAT='' BLACK='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN=''
+  fi
+
+  NF="${NOFORMAT}" BLK="${BLACK}" RED="${RED}" GRN="${GREEN}" ORG="${ORANGE}" BLU="${BLUE}" PPL="${PURPLE}" CYN="${CYAN}"
+
+  MAIN_COLOR="${NOFORMAT}"
+  HIGHLIGHT_COLOR="${GREEN}"
+}
+
+function setup_consts() {
+  readonly SCRIPT_PATH_RELATIVE="$(dirname $0)"
+  readonly SCRIPT_PATH_ABSOLUTE="$(realpath $SCRIPT_PATH_RELATIVE)"
+  readonly SCRIPT_NAME="$(basename $0)"
+  readonly CURRENT_FOLDER="$(basename $PWD)"
+  readonly RUNNING_FROM="${PWD}"
+
+  param=()
+}
+
+function throw_error() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "[$code] ${RED}${msg}${NF}"
+  exit "$code"
+}
+
+setup_colors
+
+readonly -f cleanup setup_consts setup_colors assert_var msg throw_error debug has_flag has_param get_param main
+
+##      #####
+## READ ONLY functions - END
+##      #####
+
+
+
+##      #####
+## Unsetable functions - BEGIN
+##      #####
+
+function usage() {
 
   msg "Usage: ${SCRIPT_NAME} [-h | --help] [PARAMS]"
   msg ""
-  msg "This script is just a skeleton for the others. It implements 'final' functions:"
+  msg "This script is just a skeleton for the others. It implements 'readonly' functions:"
   msg ""
   msg "  - ${CYN}cleanup${NF}"
   msg "  - ${CYN}setup_consts${NF}"
@@ -124,13 +148,13 @@ function main() {
   msg "  - ${CYN}msg${NF}"
   msg "  - ${CYN}throw_error${NF}"
   msg "  "
-  msg " and functions that may be overriden:"
+  msg " and functions that may be overriden (by unsetting them before):"
   msg ""
-  msg "  - usage"
-  msg "  - parse_params"
-  msg "  - setup_default"
-  msg "  - initialize_vars"
-  msg "  - run"
+  msg "  - ${BLU}usage${NF}"
+  msg "  - ${BLU}parse_params${NF}"
+  msg "  - ${BLU}setup_default${NF}"
+  msg "  - ${BLU}initialize_vars${NF}"
+  msg "  - ${BLU}run${NF}"
   msg ""
   msg ""
   msg "Available flags:"
@@ -139,7 +163,7 @@ function main() {
   exit
 }
 
-[[ $(type -t parse_params) != function ]] && parse_params() {
+function parse_params() {
   while :; do
     case "${1-}" in
     -h | --help)
@@ -148,7 +172,7 @@ function main() {
     --no-color)
       NO_COLOR=1
       ;;
-    ##    example of usage
+    ##    usage example
     # -a | --action)
     #   shift
     #   [[ "$1" != 'start' ]] && [[ "$1" != 'stop' ]] && [[ "$1" != 'restart' ]] && throw_error "--action must be either start, stop or restart"
@@ -171,15 +195,15 @@ function main() {
 
 }
 
-[[ $(type -t setup_default) != function ]] && setup_default() {
+function setup_default() {
   debug "${BLACK}No defaults${NF}"
 }
 
-[[ $(type -t initialize_vars) != function ]] && initialize_vars() {
+function initialize_vars() {
   debug "${BLACK}No vars were initialized${NF}"
 }
 
-[[ $(type -t run) != function ]] && run() {
+function run() {
   msg "A function named ${RED}run${NOFORMAT} must be created in order to this script to be useful"
   msg "You sould/can also override other four functions, namely:"
   msg "\t-${GREEN}usage${NOFORMAT}"
@@ -188,4 +212,10 @@ function main() {
   msg "\t-${GREEN}parse_params${NOFORMAT}\n"
 }
 
-main $@
+
+##      #####
+## Unsetable functions - END
+##      #####
+
+# main $@
+echo "sourced: $sourced"
